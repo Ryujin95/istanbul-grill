0bf5b1777cd714a0c9f363bfc0a700b4a22a126c
akkus <akkus.maxime@gmail.com>
2025-11-19 10:30:00 +0100

    chore: ignore DS_Store

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         <Route path="/desserts" element={<Desserts />} />
         <Route path="/pizzas" element={<Pizza />} />
         <Route path="/divers" element={<Divers />} />
-        <Route path="/mentions-legales" element={<MentionsLegales />} />
+        <Route path="/MentionsLegales" element={<MentionsLegales />} />
         <Route path="/cgu" element={<CGU />} />
         {/* Tu peux ajouter /pizzas, /boissons, etc. ici plus tard */}
       </Routes>
