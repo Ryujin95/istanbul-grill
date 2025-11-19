a46f78b2da2576d9350cd5e4a7ce22e2af7277d8
akkus <akkus.maxime@gmail.com>
2025-11-19 10:53:01 +0100

    chore: ignore DS_Store

--- front/src/Components/Footer/Footer.jsx
+++ front/src/Components/Footer/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div style={{ marginTop: '20px', display: 'flex', flexDirection: 'row', marginLeft: '5rem'}}>
+        <div style={{ marginTop: '20px', display: 'flex', flexDirection: 'row', marginLeft: '20rem'}}>
           <a href="/MentionsLegales" style={styles.link}>Mentions légales</a>
           <a href="/cgu" style={styles.link}>CGU</a>
         </div>
