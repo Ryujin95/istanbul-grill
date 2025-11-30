c993c73796228d116da96d520f5adf6b8bd86154
akkus <akkus.maxime@gmail.com>
2025-11-30 13:39:07 +0100

    chore: ignore DS_Store

--- back/model/Desserts.js
+++ back/model/Desserts.js
@@ -1,9 +1,9 @@
 const desserts = [
-    { nom: "Künefe", prix: "7€50",ingredients: "Pâte kadayıf, fromage filant, pistaches, sirop maison", image: "kunefe.jpg" },
-    { nom: "Tiramisu", prix: "4€50", image: "tiramisu.jpg" },
-    { nom: "Crème Brulée", prix: "4€00", image: "creme.jpg" },
-    { nom: "Tarte au Daim", prix: "3€00", image: "daim.png" },
-    { nom: "Baklava", prix: "4€50", image: "baklava.jpeg" }
+    { nom: "Künefe", prix: "7€50", ingredients: "Pâte kadayıf, fromage filant, pistaches concassées, sirop maison", image: "kunefe.jpg" },
+    { nom: "Tiramisu", prix: "4€50", ingredients: "Mascarpone, café, biscuits imbibés, cacao en poudre", image: "tiramisu.jpg" },
+    { nom: "Crème Brulée", prix: "4€00", ingredients: "Crème, vanille, sucre caramélisé", image: "creme.jpg" },
+    { nom: "Tarte au Daim", prix: "3€00", ingredients: "Fond sablé, caramel, chocolat au lait, éclats de Daim", image: "daim.png" },
+    { nom: "Baklava", prix: "4€50", ingredients: "Pâte filo, noix ou pistaches, beurre, sirop de sucre", image: "baklava.jpeg" }    
 ];
 
 module.exports = desserts;
