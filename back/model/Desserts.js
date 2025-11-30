22365dac575e8853d90b497d939cdb20fc40a680
akkus <akkus.maxime@gmail.com>
2025-11-30 13:19:18 +0100

    chore: ignore DS_Store

--- back/model/Desserts.js
+++ back/model/Desserts.js
@@ -1,5 +1,5 @@
 const desserts = [
-    { nom: "Künefe", prix: "7€50", image: "kunefe.jpg" },
+    { nom: "Künefe", prix: "7€50",ingredients: "Pâte kadayıf, fromage filant, pistaches, sirop maison", image: "kunefe.jpg" },
     { nom: "Tiramisu", prix: "4€50", image: "tiramisu.jpg" },
     { nom: "Crème Brulée", prix: "4€00", image: "creme.jpg" },
     { nom: "Tarte au Daim", prix: "3€00", image: "daim.png" },
